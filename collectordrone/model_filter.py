e5b1cdf353b6b18c530febc01919e4868fb5d75e
Frederik Schumacher <frederik.schumacher@gmail.com>
2016-06-19 10:41:21 +0200

    license stuff

--- collectordrone/model_filter.py
+++ collectordrone/model_filter.py
@@ -1,3 +1,20 @@
+# Companion web-app for Elite: Dangerous, manage blueprints and material
+# inventory for crafting engineer upgrades.
+# Copyright (C) 2016  Frederik Schumacher
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from errors import ServiceError
 from sqlalchemy import or_, and_, not_
 
@@ -30,4 +47,3 @@ def model_filter(crit, model):
             return not_(model_filter(crit.value, model))
     except AttributeError:
         raise ServiceError("invalid query field %s" % crit.field)
-
