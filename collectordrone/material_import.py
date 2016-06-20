018cb60096349930a57da49d3824b1d29f26ceef
Frederik Schumacher <frederik.schumacher@gmail.com>
2016-06-20 07:23:17 +0200

    make material import use click, add import for location from inara, database setup from config

--- collectordrone/material_import.py
+++ collectordrone/material_import.py
@@ -14,22 +14,41 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+import click
 import csv
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
+from utils import DotDict
+import yaml
 
-from models import Material
+from models import Material, Location
 
 
 def rarity(val):
-    enum = {"vcom": 0, "com": 1, "std": 2, "rare": 3, "vrare": 4}
+    enum = {
+        "vcom": 0, "com": 1, "std": 2, "rare": 3, "vrare": 4,
+        "very common": 0, "common": 1, "standard": 2, "very rare": 4
+    }
     return enum.get(val.lower(), None)
 
 
 def material_type(val):
-    enum = {"man": "manufactured", "ele": "element", "com": "commodity"}
+    enum = {
+        "man": "manufactured", "ele": "element", "com": "commodity",
+        "raw material": "element"
+    }
     return enum.get(val.lower(), val.lower())
+
+
+def location_factory(db, mat, row):
+    for location in row.get("locations", []):
+        inst = db.query(Location).filter(
+            func.lower(Location.title) == func.lower(location)
+        ).first()
+        if not inst:
+            inst = Location(title=location)
+        mat.locations.append(inst)
+        db.add(inst)
 
 
 def material_factory(db, row):
@@ -37,21 +56,47 @@ def material_factory(db, row):
         func.lower(Material.title) == func.lower(row["title"])
     ).first()
     if inst:
-        inst.type = material_type(row["type"])
-        inst.description = row["description"]
-        inst._rarity = rarity(row["rarity"])
+        if "type" in row:
+            inst.type = material_type(row["type"])
+        if "description" in row:
+            inst.description = row["description"]
+        if "rarity" in row:
+            inst._rarity = rarity(row["rarity"])
     else:
         inst = Material(
             title=row["title"],
             type=material_type(row["type"]),
-            description=row["description"],
+            description=row.get("description", ""),
             _rarity=rarity(row["rarity"])
         )
+    location_factory(db, inst, row)
     db.add(inst)
     return inst
 
 
-def material_import_edb(session, csvfile):
+def _load_config():
+    config = DotDict()
+    with open("config.yml", "r") as fp:
+        config.update(yaml.load(fp))
+    return config
+
+
+def _configure_db(config):
+    engine = create_engine(config["db.url"], echo=config["db.echo"])
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('csvfile')
+def edb_import(csvfile):
+    config = _load_config()
+    db = _configure_db(config)
     fields = (
         "title",
         "type",
@@ -61,15 +106,31 @@ def material_import_edb(session, csvfile):
     with open(csvfile, "rb") as fp:
         reader = csv.DictReader(fp, fields, restkey="extra", delimiter=",", quotechar='"')
         for row in reader:
-            material_factory(session, row)
-    session.commit()
+            material_factory(db, row)
+    db.commit()
 
 
-if __name__ == "__main__":
-    import sys
-    args = sys.argv[1:]
-    assert len(args) == 2, "material_import.py <dbpath> <csvfile>"
-    dbpath, csvfile = args
-    engine = create_engine("sqlite:///" + dbpath)
-    Session = sessionmaker(bind=engine)
-    material_import_edb(Session(), csvfile)
+@cli.command()
+@click.argument('csvfile')
+def inara_import(csvfile):
+    config = _load_config()
+    db = _configure_db(config)
+    fields = (
+        "title",
+        "type",
+        "rarity",
+        "locations",
+    )
+    with open(csvfile, "rb") as fp:
+        reader = csv.DictReader(fp, fields, restkey="extra", delimiter=";", quotechar='"')
+        for row in reader:
+            if row["locations"]:
+                row["locations"] = filter(None,
+                    (l.strip() for l in row["locations"].split(","))
+                )
+            material_factory(db, row)
+    db.commit()
+
+
+if __name__ == '__main__':
+    cli()
